64a3074ae055b6b607935c5af3626d9058d8c697
cmsbuild <>
2012-03-08 14:34:21 +0000

    This commit was manufactured by cvs2git to create tag 'SimTracker- SiPixelDigitizer-ecb20120628_Phase1Tk'.
    Sprout from SimTracker-SiPixelDigitizer-slhcupgrade_phase1_branch_4_2_X 2012-03-08 14:34:08 UTC Eric Charles Brownson <sha1-bcdc6d153210b6e6996f670d35bc71a14fc4e0d9@cern.ch> 'New values for upgrade-ROC chip inefficiencies'
    Cherrypick from master 2011-07-22 11:44:03 UTC Jamila Bashir Butt <sha1-e4df824142952b6cee4c51a55cb4ba60873d972d@cern.ch> 'added ability to read Pixel inefficiencies':
        interface/SiPixelDigitizer.h
        test/testPixelDigitizer.py

--- test/testPixelDigitizer.py
+++ test/testPixelDigitizer.py
@@ -1,4 +1,4 @@
-2# python script Pixel Digitizer testing:
+# python script Pixel Digitizer testing:
 # author: V. Cuplov
 #
 # Process available:
@@ -42,21 +42,22 @@ process.load("EventFilter.SiPixelRawToDigi.SiPixelRawToDigi_cfi")
 
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(50)
+    input = cms.untracked.int32(-1)
 )
 
-#process.MessageLogger = cms.Service("MessageLogger",
-#    debugModules = cms.untracked.vstring('PixelDigisTest'),
-#    destinations = cms.untracked.vstring('cout'),
+process.MessageLogger = cms.Service("MessageLogger",
+    debugModules = cms.untracked.vstring('SiPixelDigitizer'),
+    destinations = cms.untracked.vstring('cout'),
 #    destinations = cms.untracked.vstring("log","cout"),
-#    cout = cms.untracked.PSet(
-#        threshold = cms.untracked.string('ERROR')
-#    )
+    cout = cms.untracked.PSet(
+#       threshold = cms.untracked.string('INFO')
+#       threshold = cms.untracked.string('ERROR')
+        threshold = cms.untracked.string('WARNING')
+    )
 #    log = cms.untracked.PSet(
 #        threshold = cms.untracked.string('DEBUG')
 #    )
-#)
-
+)
 # get the files from DBS:
 #process.source = cms.Source("PoolSource",
 #    fileNames = cms.untracked.vstring('/store/relval/CMSSW_3_1_0_pre9/RelValSingleMuPt10/GEN-SIM-RECO/IDEAL_31X_v1/0007/B41EF45B-D14E-DE11-BC68-001D09F24600.root','/store/relval/CMSSW_3_1_0_pre9/RelValSingleMuPt10/GEN-SIM-RECO/IDEAL_31X_v1/0007/682FF80C-524F-DE11-9159-001D09F2543D.root','/store/relval/CMSSW_3_1_0_pre9/RelValSingleMuPt10/GEN-SIM-RECO/IDEAL_31X_v1/0007/0E9B84FD-D24E-DE11-94D9-001617C3B70E.root')
@@ -67,23 +68,66 @@ secFiles = cms.untracked.vstring()
 process.source = cms.Source("PoolSource", fileNames = readFiles, secondaryFileNames = secFiles)
 
 
-readFiles.extend( ['/store/relval/CMSSW_3_10_0_pre5/RelValSingleMuPt10/GEN-SIM-RECO/MC_39Y_V5-v1/0090/AE814955-91F5-DF11-8C95-003048678B20.root',
-        '/store/relval/CMSSW_3_10_0_pre5/RelValSingleMuPt10/GEN-SIM-RECO/MC_39Y_V5-v1/0087/8C30A969-72F5-DF11-A537-0018F3D096C8.root',
-        '/store/relval/CMSSW_3_10_0_pre5/RelValSingleMuPt10/GEN-SIM-RECO/MC_39Y_V5-v1/0085/C2E568D8-0DF5-DF11-9EA2-0026189437F5.root',
-        '/store/relval/CMSSW_3_10_0_pre5/RelValSingleMuPt10/GEN-SIM-RECO/MC_39Y_V5-v1/0085/9A402DD9-15F5-DF11-87D6-003048D15E24.root',
-        '/store/relval/CMSSW_3_10_0_pre5/RelValSingleMuPt10/GEN-SIM-RECO/MC_39Y_V5-v1/0085/28199BD0-12F5-DF11-9A35-001A92971B5E.root',
-        '/store/relval/CMSSW_3_10_0_pre5/RelValSingleMuPt10/GEN-SIM-RECO/MC_39Y_V5-v1/0085/023672FD-0DF5-DF11-AE40-003048678CA2.root'] );
+readFiles.extend( [
+###Muons ls -ltr /scratch/jbutt/MuPt100/* | awk '{print ",\x27 file:" $9 "\x27 "}'
+# 'file:/scratch/jbutt/MuPt100/9889CC05-8261-E011-94F7-001A92811738.root '
+#,'file:/scratch/jbutt/MuPt100/00AEB646-AD60-E011-A906-00261894394D.root '
+###QCD ls -ltr /scratch/jbutt/QCD80120/* | awk '{print ",\x27 file:" $9 "\x27 "}'
+#  ' file:/scratch/jbutt/QCD80120/F8DCFBB7-4261-E011-B677-0026189438A9.root' 
+# ,' file:/scratch/jbutt/QCD80120/0A209954-B460-E011-AD7D-002618943870.root' 
+# ,' file:/scratch/jbutt/QCD80120/CC353A09-9B60-E011-B000-0026189438AA.root' 
+# ,' file:/scratch/jbutt/QCD80120/7CF0CF1A-9D60-E011-9BEF-002618943884.root' 
+# ,' file:/scratch/jbutt/QCD80120/70890C93-A160-E011-BFF6-003048678B5E.root' 
+###TTbar  ls -ltr /scratch/jbutt/TTbar/* | awk '{print ",\x27 file:" $9 "\x27 "}'
+ ' file:/scratch/jbutt/TTbar/52278ED2-4161-E011-9FDE-002618943934.root' 
+,' file:/scratch/jbutt/TTbar/D863DB68-BA60-E011-9121-0018F3D09702.root' 
+,' file:/scratch/jbutt/TTbar/984417B1-A960-E011-98A2-001A92811736.root' 
+,' file:/scratch/jbutt/TTbar/DEA836EC-9060-E011-A388-003048678D52.root' 
+#,' file:/scratch/jbutt/TTbar/8294BF95-9260-E011-B485-002354EF3BD2.root' 
+#,' file:/scratch/jbutt/TTbar/169EAF9F-8C60-E011-82B6-00248C0BE005.root' 
+ ] );
 
-secFiles.extend( ['/store/relval/CMSSW_3_10_0_pre5/RelValSingleMuPt10/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_39Y_V5-v1/0090/06D49A63-91F5-DF11-B2CE-00261894380B.root',
-        '/store/relval/CMSSW_3_10_0_pre5/RelValSingleMuPt10/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_39Y_V5-v1/0086/E0F513AA-2BF5-DF11-863B-001A92810A96.root',
-        '/store/relval/CMSSW_3_10_0_pre5/RelValSingleMuPt10/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_39Y_V5-v1/0085/F85C3C87-0BF5-DF11-ABEC-001A92971B8C.root',
-        '/store/relval/CMSSW_3_10_0_pre5/RelValSingleMuPt10/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_39Y_V5-v1/0085/CC8F5B4B-17F5-DF11-8419-002354EF3BDF.root',
-        '/store/relval/CMSSW_3_10_0_pre5/RelValSingleMuPt10/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_39Y_V5-v1/0085/C046BD12-11F5-DF11-BBE4-0026189438A7.root',
-        '/store/relval/CMSSW_3_10_0_pre5/RelValSingleMuPt10/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_39Y_V5-v1/0085/5A335988-14F5-DF11-805C-003048679048.root',
-        '/store/relval/CMSSW_3_10_0_pre5/RelValSingleMuPt10/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_39Y_V5-v1/0085/4C5E3F2D-12F5-DF11-B24C-0018F3D096A6.root',
-        '/store/relval/CMSSW_3_10_0_pre5/RelValSingleMuPt10/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_39Y_V5-v1/0085/4A2036B3-07F5-DF11-A8AC-001A92971BDC.root',
-        '/store/relval/CMSSW_3_10_0_pre5/RelValSingleMuPt10/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_39Y_V5-v1/0085/385FE049-0DF5-DF11-BFE1-00248C0BE016.root',
-        '/store/relval/CMSSW_3_10_0_pre5/RelValSingleMuPt10/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_39Y_V5-v1/0085/08E5C3EC-12F5-DF11-9B5C-003048679084.root'] )
+secFiles.extend( [
+####Muons
+# 'file:/scratch/jbutt/MuPt100/CA754A24-6361-E011-9CC8-00304867923E.root '
+#,'file:/scratch/jbutt/MuPt100/D6CFE91F-A460-E011-A535-0018F3D096BC.root '
+#,'file:/scratch/jbutt/MuPt100/666B6830-A360-E011-9A15-0018F3D0961A.root '
+###QCD
+#  ' file:/scratch/jbutt/QCD80120/90D1BF5E-8261-E011-90FF-001A92810AA4.root' 
+# ,' file:/scratch/jbutt/QCD80120/B66E35F6-B760-E011-8374-00304867924E.root' 
+# ,' file:/scratch/jbutt/QCD80120/EAD73409-9F60-E011-AF29-0026189438FD.root' 
+# ,' file:/scratch/jbutt/QCD80120/D820C5B6-A260-E011-A96A-002618FDA262.root' 
+# ,' file:/scratch/jbutt/QCD80120/D2BDC039-A160-E011-B201-003048678E24.root' 
+# ,' file:/scratch/jbutt/QCD80120/CC774519-9D60-E011-8618-002618943970.root' 
+# ,' file:/scratch/jbutt/QCD80120/AC090BAB-A660-E011-826E-001A9281172C.root' 
+# ,' file:/scratch/jbutt/QCD80120/8C1F0E90-9B60-E011-B8D7-001A92971B5E.root' 
+# ,' file:/scratch/jbutt/QCD80120/7E592C4A-B060-E011-8FE0-00261894391C.root' 
+# ,' file:/scratch/jbutt/QCD80120/749CCA7F-9960-E011-9D50-003048679166.root' 
+# ,' file:/scratch/jbutt/QCD80120/6A5B1394-A060-E011-AC40-00304867906C.root' 
+# ,' file:/scratch/jbutt/QCD80120/526CEC1F-9D60-E011-8286-001A92971B74.root' 
+# ,' file:/scratch/jbutt/QCD80120/4EA249A0-9C60-E011-B0E0-002618943945.root' 
+# ,' file:/scratch/jbutt/QCD80120/3252A9B3-9A60-E011-A3C7-00261894386C.root' 
+# ,' file:/scratch/jbutt/QCD80120/1CCA2929-9D60-E011-BD31-0026189438E4.root' 
+###TTbar
+ ' file:/scratch/jbutt/TTbar/FCB768B7-4261-E011-B6A9-00261894396D.root' 
+,' file:/scratch/jbutt/TTbar/FED391BB-BB60-E011-B5E7-0018F3D09670.root' 
+,' file:/scratch/jbutt/TTbar/8E1219D2-B360-E011-AEC0-0018F3D0962E.root' 
+,' file:/scratch/jbutt/TTbar/8C5378DD-BE60-E011-BCEF-001A92810AE2.root' 
+,' file:/scratch/jbutt/TTbar/FA891A2E-A060-E011-B60E-00304867906C.root' 
+,' file:/scratch/jbutt/TTbar/CC2DC499-A160-E011-98B2-002618943862.root' 
+,' file:/scratch/jbutt/TTbar/BADE8A9F-9A60-E011-8DC6-001BFCDBD176.root' 
+,' file:/scratch/jbutt/TTbar/9885A433-9560-E011-862D-0026189438A5.root' 
+,' file:/scratch/jbutt/TTbar/8A62C3C1-9B60-E011-B4D3-001A92810AEE.root' 
+,' file:/scratch/jbutt/TTbar/8A00EDE3-B160-E011-A4BA-0018F3D0968C.root' 
+,' file:/scratch/jbutt/TTbar/762DC67E-9D60-E011-96D3-0026189438E8.root' 
+,' file:/scratch/jbutt/TTbar/6A5F5891-A060-E011-B3AC-003048679150.root' 
+,' file:/scratch/jbutt/TTbar/2E43CDFD-9960-E011-A5E1-00261894392C.root' 
+,' file:/scratch/jbutt/TTbar/2A3EAE9F-A760-E011-AC4A-003048678B06.root' 
+,' file:/scratch/jbutt/TTbar/2493B4C4-9660-E011-A92F-00304867BFAA.root' 
+,' file:/scratch/jbutt/TTbar/1A057A0C-9A60-E011-8A9B-002618943986.root' 
+,' file:/scratch/jbutt/TTbar/10656835-9B60-E011-8A77-0018F3D0964A.root' 
+,' file:/scratch/jbutt/TTbar/084C200A-9960-E011-83F6-00261894386E.root' 
+        ] )
 
 
 
@@ -104,7 +148,7 @@ secFiles.extend( ['/store/relval/CMSSW_3_10_0_pre5/RelValSingleMuPt10/GEN-SIM-DI
 
 # Choose the global tag here:
 #process.GlobalTag.globaltag = 'MC_38Y_V8::All'
-process.GlobalTag.globaltag = 'MC_39Y_V5::All'
+process.GlobalTag.globaltag = 'MC_42_V11::All'
 
 process.load("CondCore.DBCommon.CondDBSetup_cfi")
 
@@ -115,8 +159,8 @@ process.load("CondCore.DBCommon.CondDBSetup_cfi")
 
 process.o1 = cms.OutputModule("PoolOutputModule",
                               outputCommands = cms.untracked.vstring('drop *','keep *_*_*_DigiTest'),
-            fileName = cms.untracked.string('rfio:/castor/cern.ch/user/v/vesna/testDigis.root')
-#            fileName = cms.untracked.string('file:dummy.root')
+#            fileName = cms.untracked.string('rfio:/castor/cern.ch/user/v/vesna/testDigis.root')
+            fileName = cms.untracked.string('file:dummy.root')
 )
 
 process.Timing = cms.Service("Timing")
@@ -143,7 +187,7 @@ process.trackinghits = cms.Sequence(process.TrackRefitter*process.trackingRecHit
 #process.p1 = cms.Path(process.mix*process.digis*process.siPixelRawData*process.siPixelDigis*process.pixeltrackerlocalreco)
 
 #This process to get events to be used to make DQM occupancy maps (cmsRun DQM_Pixel_digi.py after you ran testPixelDigitizer.py):
-process.p1 = cms.Path(process.mix*process.simSiPixelDigis*process.siPixelRawData*process.siPixelDigis)
+#process.p1 = cms.Path(process.mix*process.simSiPixelDigis*process.siPixelRawData*process.siPixelDigis)
 
 # Look at cluster charge:
 #process.p1 = cms.Path(process.mix*process.digis*process.siPixelRawData*process.siPixelDigis*process.pixeltrackerlocalreco)
@@ -152,7 +196,7 @@ process.p1 = cms.Path(process.mix*process.simSiPixelDigis*process.siPixelRawData
 #process.p1 = cms.Path(process.mix*process.simSiPixelDigis*process.pixelDigisValid)
 
 #This process is to run the digitizer:
-#process.p1 = cms.Path(process.mix*process.simSiPixelDigis)
+process.p1 = cms.Path(process.mix*process.simSiPixelDigis)
 
 process.outpath = cms.EndPath(process.o1)
 process.g4SimHits.Generator.HepMCProductLabel = 'source'
